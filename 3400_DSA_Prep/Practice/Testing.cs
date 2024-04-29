a04218421daf710e8bbcdd0c5411f7aff57dc55b
Ryan <hawkinsry19@gmail.com>
2024-04-29 12:09:28 -0400

    Testing class created

--- 3400_DSA_Prep/Practice/Testing.cs
+++ 3400_DSA_Prep/Practice/Testing.cs
@@ -11,16 +11,59 @@ namespace _3400_DSA_Prep.Practice
     public static class Testing
     {
         // Use this class to test methods you write while practicing 
+        // or don't (lol)
 
+
+        // NOTE TestCase Constructor 
+        // Title: Title of the test case
+        // ExpectedOutput: The expected output of the test case
+        // Collection: The collection to generate and test
+
+        private static TestCase[] GenerateCases()
+        {
+            // Use this method to generate test cases
+            TestCase[] cases = new TestCase[]
+            {
+                new TestCase(
+                    title: "Empty Case",
+                    expectedOutput: "false",
+                    collection: new int[] { }
+                ),
+                new TestCase(
+                    title: "Single Element",
+                    expectedOutput: "true",
+                    collection: new int[] { 1 }
+               ),
+                new TestCase("Working Case", "True", new int[] { 1, 2, 3, 4, 5 }),
+            };
+            return cases;
+        }
+
+        public static void RunTests()
+        {
+            var cases = GenerateCases();
+            foreach (var c in cases)
+            {
+                // replace with class your testing
+                var collection = Generator.LinkedList(c.Collection);
+
+                var result = collection.Contains(1); // call method your testing 
+                Console.WriteLine(c);
+                Console.WriteLine($"Actual Output: {result}\n");
+            }
+        }
 
     }
+
+
+
 
 
     // use this to generate test cases
     public static class Generator
     {
 
-        public static OurList<int> GenerateLinkedList(int[] thingsToAdd)
+        public static OurList<int> LinkedList(int[] thingsToAdd)
         {
             var list = new OurList<int>();
             for (int i = 0; i < thingsToAdd.Length; i++)
@@ -30,7 +73,7 @@ namespace _3400_DSA_Prep.Practice
             return list;
         }
 
-        public static CircularQueue<int> GenerateCircularQueue(int[] thingsToAdd)
+        public static CircularQueue<int> CircularQueue(int[] thingsToAdd)
         {
             var queue = new CircularQueue<int>(thingsToAdd.Length);
             for (int i = 0; i < thingsToAdd.Length; i++)
@@ -40,7 +83,7 @@ namespace _3400_DSA_Prep.Practice
             return queue;
         }
 
-        public static OurStack<int> GenerateStack(int[] thingsToAdd)
+        public static OurStack<int> Stack(int[] thingsToAdd)
         {
             var stack = new OurStack<int>();
             for (int i = 0; i < thingsToAdd.Length; i++)
@@ -50,7 +93,7 @@ namespace _3400_DSA_Prep.Practice
             return stack;
         }
 
-        public static PriorQ<int, int> GeneratePrioQ(int[] thingsToAdd)
+        public static PriorQ<int, int> PrioQ(int[] thingsToAdd)
         {
             var pq = new PriorQ<int, int>();
             for (int i = 0; i < thingsToAdd.Length; i++)
@@ -60,7 +103,7 @@ namespace _3400_DSA_Prep.Practice
             return pq;
         }
 
-        public static Dict<int, int> GenerateDictionary(int[] keys, int[] values = null)
+        public static Dict<int, int> Dictionary(int[] keys, int[] values = null)
         {
 
             if (values == null)
@@ -84,7 +127,7 @@ namespace _3400_DSA_Prep.Practice
             return d;
         }
 
-        public static BST<int> GenerateBST(int[] thingsToAdd)
+        public static BST<int> BST(int[] thingsToAdd)
         {
             if (thingsToAdd.ToHashSet().Count != thingsToAdd.Length)
             {
@@ -98,7 +141,7 @@ namespace _3400_DSA_Prep.Practice
             return d;
         }
 
-        public static AVL<int> GenerateAVL(int[] thingsToAdd)
+        public static AVL<int> AVL(int[] thingsToAdd)
         {
             if (thingsToAdd.ToHashSet().Count != thingsToAdd.Length)
             {
@@ -111,8 +154,25 @@ namespace _3400_DSA_Prep.Practice
             }
             return d;
         }
+    }
 
+    public class TestCase
+    {
+        public string Title { get; set; }
+        public string ExpectedOutput { get; set; }
+        public int[] Collection { get; set; }
+        public string For { get; set; }
 
+        public TestCase(string title, string expectedOutput, int[] collection)
+        {
+            Title = title;
+            ExpectedOutput = expectedOutput;
+            Collection = collection;
+        }
 
+        public override string ToString()
+        {
+            return $"Case {Title}\nExpected Output: {ExpectedOutput}\nItems: {string.Join(", ", Collection)}";
+        }
     }
 }
